be6eb33e97fca349b1e62a41f377ec0c7d4df8ad
Claudio Bustos <clbustos@gmail.com>
2010-11-02 10:31:57 -0300

    Version 0.2.0

--- lib/rubyvis.rb
+++ lib/rubyvis.rb
@@ -26,9 +26,9 @@ require 'rubyvis/transform'
 module Rubyvis
   @document=nil
   # Rubyvis version
-  VERSION = '0.1.7' 
+  VERSION = '0.2.0' 
   # Protovis API on which current Rubyvis is based
-  API_VERSION='3.3'
+  PROTOVIS_API_VERSION='3.3'
   # You actually can do it! http://snipplr.com/view/2137/uses-for-infinity-in-ruby/
   Infinity=1.0 / 0 
   #
