4030f6a9afda83ce4466212719bb2d767e3ac48a
Claudio Bustos <clbustos@gmail.com>
2010-11-12 00:57:34 -0300

    Updated antibiotics example

--- examples/antibiotics/antibiotics_wedge.rb
+++ examples/antibiotics/antibiotics_wedge.rb
@@ -18,14 +18,14 @@ gram_color = {
   :negative=> "rgba(230, 130, 110, .8)"
 }
 
-#/* Burtin's radius encoding is, as far as I can tell, sqrt(log(mic)). */
+# Burtin's radius encoding is, as far as I can tell, sqrt(log(mic)).
 min = Math.sqrt(Math.log(0.001 * 1e4))
 max = Math.sqrt(Math.log(1000 * 1e4))
 a = (outer_radius - inner_radius) / (min - max.to_f)
 b = inner_radius - a * max
 
 radius = lambda {|mic| a * Math.sqrt(Math.log(mic * 1e4)) + b}
-# 
+
 # The pie is split into equal sections for each bacteria, with a blank
 # section at the top for the grid labels. Each wedge is further
 # subdivided to make room for the three antibiotics, equispaced.
@@ -40,7 +40,7 @@ vis = Rubyvis::Panel.new.
 
 
 
-#/* Background wedges to indicate gram staining color. */
+# Background wedges to indicate gram staining color.
 bg = vis.add(Rubyvis::Wedge).
   data($bacteria). # assumes Burtin's order
   left(lambda {|i| width / 2.0}).
@@ -48,37 +48,56 @@ bg = vis.add(Rubyvis::Wedge).
   inner_radius(inner_radius).
   outer_radius(outer_radius).
   angle(big_angle).
-  start_angle(lambda {|d| self.index * big_angle + big_angle / 2.0 - Math::PI / 2.0}).
-  fill_style(lambda {|d| Rubyvis.color(gram_color[d[:gram].to_sym])})
+  start_angle(lambda {|d| 
+  index * big_angle + big_angle / 2.0 - Math::PI / 2.0
+  }).
+  fill_style(lambda {|d|
+  Rubyvis.color(gram_color[d[:gram].to_sym])
+  })
 
-  # Antibiotics. 
+# Antibiotics. 
+
 bg.add(Rubyvis::Wedge).
   angle(small_angle).
-  start_angle(lambda {|d| bg.start_angle() + small_angle}).
-  outer_radius(lambda {|d| radius.call(d[:penicillin])}).
+  start_angle(lambda {|d| 
+    bg.start_angle() + small_angle
+  }).
+  outer_radius(lambda {|d| 
+  radius.call(d[:penicillin])
+  }).
   fill_style(drug_color[:penicillin]).
     add(Rubyvis::Wedge).
-    start_angle(lambda {|d| self.proto.start_angle() + 2 * small_angle}).
-    outer_radius(lambda {|d| radius.call(d[:streptomycin])}).
+    start_angle(lambda {|d| 
+    proto.start_angle() + 2 * small_angle}).
+    outer_radius(lambda {|d| 
+      radius.call(d[:streptomycin])
+    }).
     fill_style(drug_color[:streptomycin]).
     add(Rubyvis::Wedge).
-    outer_radius(lambda {|d| radius.call(d[:neomycin])}).
+    outer_radius(lambda {|d|
+      radius.call(d[:neomycin])
+    }).
     fill_style(drug_color[:neomycin])
 
-#/* Circular grid lines. */
+# Circular grid lines. 
 
 bg.add(Rubyvis::Dot)
     .data(Rubyvis.range(-3, 4))
     .fill_style(nil)
     .stroke_style("#eee")
     .line_width(1)
-    .shape_size(lambda {|i| radius.call(10**i)** 2})
-  .anchor("top").add(Rubyvis::Label).
-    visible(lambda {|i| i < 3})
-    .text_baseline("middle")
-    .text(lambda {|i| i<0 ? (10** i).to_f : (10**i).to_i})
+    .shape_size(lambda {|i| 
+      radius.call(10**i)** 2
+    }).
+  anchor("top").add(Rubyvis::Label).
+    visible(lambda {|i| i < 3}).
+    text_baseline("middle").
+    text(lambda {|i| 
+      i<0 ? (10** i).to_f : (10**i).to_i
+    })
 
-#/* Radial grid lines. */
+# Radial grid lines. 
+
 bg.add(Rubyvis::Wedge)
 .data(Rubyvis.range($bacteria.size + 1))
     .inner_radius(inner_radius - 10)
@@ -87,12 +106,12 @@ bg.add(Rubyvis::Wedge)
     .stroke_style("black")
     .angle(0)
 
-#/* Labels. */
+# Labels. 
 bg.anchor("outer").add(Rubyvis::Label)
     .text_align("center")
-    .text(lambda {|d| d[:bacteria]})
+    .text(lambda {|d| d[:name]})
 
-#/* Antibiotic legend. */
+# Antibiotic legend. 
 vis.add(Rubyvis::Bar)
     .data(Rubyvis.keys(drug_color))
     .right(width / 2 + 3)
@@ -104,18 +123,18 @@ vis.add(Rubyvis::Bar)
     .textMargin(6)
     .textAlign("left");
 
-#/* Gram-stain legend. */
-vis.add(pv.Dot)
-  .data([:positive, :negative])
-    .left(width / 2.0 - 20)
-    .bottom(lambda { -60 + self.index * 18})
-    .fill_style(lambda {|d| gram_color[d]})
-    .stroke_style(nil)
-    .shape_size(30)
-  .anchor("right").add(Rubyvis::Label)
-  .text_margin(6)
-    .text_align("left")
-    .text(lambda {|d| "Gram-#{d}"})
+# Gram-stain legend.
+vis.add(Rubyvis::Dot).
+  data([:positive, :negative]).
+  left(width / 2.0 - 20).
+  bottom(lambda { -60 + index * 18}).
+  fill_style(lambda {|d| gram_color[d]}).
+  stroke_style(nil).
+  shape_size(30).
+  anchor("right").add(Rubyvis::Label).
+    text_margin(6).
+    text_align("left").
+    text(lambda {|d| "Gram-#{d}"})
 
 vis.render
 puts vis.to_svg
