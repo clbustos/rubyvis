d748dda796c57b41e362cf15dc4bf7d47d26f3f5
Claudio Bustos <clbustos@gmail.com>
2011-01-26 13:23:12 -0300

    Updated to V0.4.1

--- lib/rubyvis.rb
+++ lib/rubyvis.rb
@@ -32,7 +32,7 @@ require 'rubyvis/mark/shorcut_methods'
 module Rubyvis
   @document=nil
   # Rubyvis version
-  VERSION = '0.4.0' 
+  VERSION = '0.4.1' 
   # Protovis API on which current Rubyvis is based
   PROTOVIS_API_VERSION='3.3'
   # You actually can do it! http://snipplr.com/view/2137/uses-for-infinity-in-ruby/
